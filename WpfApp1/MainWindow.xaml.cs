82ce38d5887de44923e87d21961d2b8ef4394ddd
BeeksGM <gabriel.monroig@Gmail.com>
2020-11-13 11:01:10 -0500

    Upload of Standard Login Screen Logic
    Has Check for Password and Username
    No Database Attached (can do)
    Hidden Password
    25 Minutes to Complete.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string AdminUser = "admin";
+        string AdminPass = "wpix11";
+
+        string UserName = "news";
+        string UserPass = "wpix11";
+
+        
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+       
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            UserAuth();
+        }
+
+        private void Grid_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key.ToString() == "Return")
+            {
+                UserAuth();
+            }
+
+
+        }
+
+        private void UserAuth()
+        {
+            string EnteredUsername = Username.Text;
+            string EnteredPassword = Password.Text;
+
+            if (EnteredUsername == AdminUser)
+            {
+                if (EnteredPassword == AdminPass)
+                {
+                    MessageBox.Show("Welcome Admin!");
+
+                }
+                else
+                {
+                    MessageBox.Show("Admin Password Incorrect!");
+                }
+
+            }
+            else if (EnteredUsername == UserName)
+            {
+                if(EnteredPassword == UserPass)
+                {
+                    
+                    MessageBox.Show($"Welcome {UserName} !");
+
+                }
+                else
+                {
+                    MessageBox.Show("You entered the Incorrect Password.");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show($"The Username {EnteredUsername}, Does not match any Username");
+            }
+
+        }
+    }
+}
+
+
